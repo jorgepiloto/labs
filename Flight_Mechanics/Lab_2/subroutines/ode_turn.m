4c124bc60bd8e130c87b6a30aec67fad0deba625
Jorge <ingenierodeaviones@gmail.com>
2019-04-14 18:13:58 +0200

    Turn maneuver

--- Flight_Mechanics/Lab_2/subroutines/ode_turn.m
+++ Flight_Mechanics/Lab_2/subroutines/ode_turn.m
@@ -2,7 +2,7 @@ function dudt = ode_turn(t, u, xc)
 
 global p;
 g = 9.8; 
-% u = [V, x, y, h, ji, W]
+% u = [V, x, y, h, chi, W]
 % Retrieve parameters
 S = p(2);
 CD0 = p(5);
@@ -15,7 +15,7 @@ V = u(1);
 x = u(2);
 y = u(3);
 h = u(4);
-ji = u(5);
+chi = u(5);
 W = u(6);
 
 % Control vector
@@ -34,8 +34,8 @@ L = 1/2 * rho * V ^ 2 * CL;
 % Get the time derivatives
 dudt = zeros(6,1);
 dudt(1) = (T - D) * g / W;
-dudt(2) = V * cos(ji);
-dudt(3) = V * sin(ji);
+dudt(2) = V * cos(chi);
+dudt(3) = V * sin(chi);
 dudt(4) = 0;
 dudt(5) = L * g * sin(mu) / W / V;
 dudt(6) = -SFCT * T;
