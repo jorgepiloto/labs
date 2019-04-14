a62598769fdcbc1ae84bf7adb87667e3001ffd5a
Jorge <ingenierodeaviones@gmail.com>
2019-04-14 23:03:12 +0200

    Bug is solved

--- Flight_Mechanics/Lab_2/subroutines/ode_turn.m
+++ Flight_Mechanics/Lab_2/subroutines/ode_turn.m
@@ -1,43 +1,67 @@
 function dudt = ode_turn(t, u, xc)
 
 global p;
-g = 9.8; 
-% u = [V, x, y, h, chi, W]
-% Retrieve parameters
-S = p(2);
-CD0 = p(5);
-k = p(9);
-TmaxSL = p(6);
-SFCT = p(7);
 
-% Variables
+% Vector u = [V, x, h, W, y, chi]
 V = u(1);
 x = u(2);
-y = u(3);
-h = u(4);
-chi = u(5);
-W = u(6);
+h = u(3);
+W = u(4);
+y = u(5);
+chi = u(6);
 
-% Control vector
+% Parameters
+S = p(2);
+Cd0 = p(5);
+k = p(9);
+SFC = p(7);
+g = 9.81;
+Tmax = p(6);
+
+% Densities
+rho = ISA(h);
+rho0 = ISA(0);
+
+% Control variables
+xc = fun_control(1, p);
 PI = xc(1);
 mu = xc(2);
 
-% Get state related variables
-rhoSL = ISA(0);
-rho = ISA(h);
-T = PI * TmaxSL * (rho / rhoSL) ^ 0.7;
-CL = W / (1/2 * rho * V ^ 2) / cos(mu);
-D = 1/2 * rho * V ^ 2 * S * (CD0 + k * CL ^ 2);
+% Compute forces
+CL = 2 * W / (cos(mu) * rho * S * V ^ 2); 
+T = PI * Tmax * (rho / rho0) ^ 0.7;
+L = 0.5 * rho * V ^ 2 * S * CL;
+CD = Cd0 + k * CL ^ 2;
+D = 0.5 * rho * V ^ 2 * S * CD;
 
-L = 1/2 * rho * V ^ 2 * CL;
+% Derivatives
+% Vector u = [V, x, h, W, y, chi]
+dudt = zeros(6, 1);
+dudt(1) = (T - D) * g / W;     
+dudt(2) = V * cos(chi);          
+dudt(3) = 0;                   
+dudt(4) = -SFC * T;            
+dudt(5) = V * sin(chi);
+dudt(6) = -(L / W) * (g / V) * sin(mu); 
 
-% Get the time derivatives
-dudt = zeros(6,1);
-dudt(1) = (T - D) * g / W;
-dudt(2) = V * cos(chi);
-dudt(3) = V * sin(chi);
-dudt(4) = 0;
-dudt(5) = L * g * sin(mu) / W / V;
-dudt(6) = -SFCT * T;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 end
--- Flight_Mechanics/Lab_2/turn.m
+++ Flight_Mechanics/Lab_2/turn.m
@@ -6,7 +6,7 @@ global p
 % Loading all the parameters and initial conditions
 p = parameters();
 
-%% Generating contour plots
+% Generating contour plots
 vspan_hat = 0.1:0.1:2.5;
 Tspan_hat = 0.1:0.1:4;
 [V_hat, T_hat] = meshgrid(vspan_hat, Tspan_hat);
@@ -25,59 +25,46 @@ for i=1:length(contour_titles)
     colormap(autumn);
     title(contour_titles(i));
 end
+close all;
 
-%% Compute PI and bank angle
-h0 = 10000;
-V_hat = 1.1;
+% Solve ODE
 T_hat = 1.2;
+V_hat = 1.1;
+h0 = 10000;
+[T0, V0, Tb, Vb] = from_nd_2_d(T_hat, V_hat, p, h0, p(8));
 
-mu = acos(1 / T_hat);
-[T0, V0, ~, ~] = from_nd_2_d(T_hat, V_hat, p, h0, p(8));
-
-PI = (T0 / p(6)) * (ISA(0) / ISA(h0)) ^ 0.7;
-
-%% Solving ode_turn
-tspan = [0 5*3600];
+xc = fun_control(2, p);
+tspan = [0 18000];
 x0 = 0;
 y0 = 0;
-ji0 = 0;
-u0 = [V0, x0, y0, h0, ji0, p(8)];
-xc = [PI, mu];
+chi0 = 0;
 
-[t, u] = ode45(@(t, u) ode_turn(t, u, xc), tspan, u0); % Evolution of system
+% Vector u = [V, x, h, W, y, chi]
+u0 = [V0, x0, h0, p(8), y0, chi0];
+
+[t, u] = ode45(@(t, u) ode_turn(t, u, xc), tspan, u0, odeset('RelTol',1e-10)); % Evolution of system
 
 %% Ploting all the results
 
 plot_title = ["Velocity", "\chi", "W"];
 plot_colors = ['b', 'r', 'k'];
 
+% Vector u = [V, x, h, W, y, chi]
 figure();
-vars = {u(:,1), u(:,5), u(:,6)};
+vars = {u(:,1), u(:,6), u(:,4)};
 for i=1:3
     subplot(4, 1, i)
     plot(t, vars{i}, plot_colors(i), 'linewidth', 1.5);
     title(plot_title(i));
     xlabel("Time [s]")
 end
-subplot(4, 1, 4)
-plot(u(:,3), u(:,2), 'y');
+
+figure()
+plot(u(:,2), u(:,5), 'y');
 xlabel("Time [s]");
 title("X vs Y");
 
-% Plotting T_hat vs V_hat
-T_max = p(6);
-T = PI * T_max * (ISA(h0) / ISA(0)) ^ 0.7 * ones(length(u(:, 3)));
 
-figure()
-[T_hat, V_hat, T_b, V_b] = from_d_2_nd(T, u(:, 1), p, u(:, 4), u(:, 6));
-plot(V_hat, T_hat, 'b', 'linewidth', 1.5);
-hold on;
-V_hat = 0.1:0.1:3.0;
-T_hat = 1/2 .* (V_hat .^ 2 + 1 ./ V_hat .^ 2 / cos(mu) .^ 2);
-plot(V_hat, T_hat, 'k', 'linewidth', 1.5);
-title("T_h vs V_h");
-xlabel("V_h");
-ylabel("T_h");
 
 
     
